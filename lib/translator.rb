dd8810869680cac61da21d95a58a50beee02b20d
Cedric Patton <ceddyp117@gmail.com>
2020-07-04 22:54:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,7 @@ def get_japanese_emoticon(file_path, emoticon)
   emoticon = library.keys.find do |key|
     library[key][:english] == emoticon
   end
+  
   emoticon ? library[emoticon][:japanese] : "Sorry, that emoticon was not found"
 end
 
@@ -27,5 +28,6 @@ def get_english_meaning(file_path, emoticon)
   emoticon = library.keys.find do |key|
     library[key][:japanese] == emoticon
   end
+  
   emoticon ? emoticon : "Sorry, that emoticon was not found"
 end
